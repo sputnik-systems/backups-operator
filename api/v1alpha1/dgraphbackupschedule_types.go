9546309e2751ceebad9a6553954eab80ceb89cce
Ildar Valiullin <preved.911@gmail.com>
2021-11-26 11:35:10 +0300

    added columns into additional printed columns list

--- api/v1alpha1/dgraphbackupschedule_types.go
+++ api/v1alpha1/dgraphbackupschedule_types.go
@@ -41,6 +41,8 @@ type DgraphBackupScheduleStatus struct {
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:name="Schedule",type="string",JSONPath=".spec.schedule",description="backup objects creation schedule"
+//+kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 
 // DgraphBackupSchedule is the Schema for the dgraphbackupschedules API
 type DgraphBackupSchedule struct {
